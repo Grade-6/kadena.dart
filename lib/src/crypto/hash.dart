60a724231db2f9f01c06f580ae0436bba33196a5
kleak <me@kleak.dev>
2022-01-08 18:01:05 +0100

    delete debug log

--- lib/src/crypto/hash.dart
+++ lib/src/crypto/hash.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:pinenacl/digests.dart';
 
 String hash(String message) {
-  print(message);
   final hash = Hash.blake2b(message, digestSize: 32);
   return base64Url.encode(hash).replaceAll('=', '');
 }
